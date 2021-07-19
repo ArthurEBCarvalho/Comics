5efea606e2991e641b6a68966f8465f7b4f7a74a
Arthur Emanuel Brandão Carvalho <arthur_aebc@hotmail.com>
2021-07-19 17:35:38 -0300

    params with page

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,11 +1,15 @@
 class ComicsController < ApplicationController
   def index
-    @comics = Marvel::Fetch.comics(comic_params[:character])
+    @comics = Marvel::Fetch.comics(fetch_comics_params)
   end
 
   private
 
   def comic_params
-    params.permit(:character)
+    params.permit(:character, :page)
+  end
+
+  def fetch_comics_params
+    { character_name: comic_params[:character], page: comic_params[:page] }.compact
   end
 end
--- spec/controllers/comics_controller_spec.rb
+++ spec/controllers/comics_controller_spec.rb
@@ -16,14 +16,34 @@ RSpec.describe ComicsController, type: :controller do
       before { get :index }
   
       it { is_expected.to have_http_status(:success) }
-      it { expect(Marvel::Fetch).to have_received(:comics) }
+      it { expect(Marvel::Fetch).to have_received(:comics).with({}) }
     end
 
-    context 'when has param' do
+    context 'when has character param' do
       before { get :index, params: { character: :Deadpool } }
 
       it { is_expected.to have_http_status(:success) }
-      it { expect(Marvel::Fetch).to have_received(:comics).with('Deadpool') }
+      it { expect(Marvel::Fetch).to have_received(:comics).with(character_name: 'Deadpool') }
+    end
+
+    context 'when has page param' do
+      before { get :index, params: { page: page } }
+
+      (1..3).each do |page|
+        context "when has page #{page}" do
+          let(:page) { page }
+  
+          it { is_expected.to have_http_status(:success) }
+          it { expect(Marvel::Fetch).to have_received(:comics).with(page: page.to_s) }
+        end
+      end
+    end
+
+    context 'when has both params' do
+      before { get :index, params: { character: :Deadpool, page: '2' } }
+
+      it { is_expected.to have_http_status(:success) }
+      it { expect(Marvel::Fetch).to have_received(:comics).with(character_name: 'Deadpool', page: '2') }
     end
   end
 end
