6486e5c254ecf7b76fdb2e1637f3c66c7b55ecf1
Arthur Emanuel Brandão Carvalho <arthur_aebc@hotmail.com>
2021-07-20 19:38:13 -0300

    Favorites (#6)

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,15 +1,41 @@
 class ComicsController < ApplicationController
+  respond_to :json, only: %i[add_favorite remove_favorite]
+
   def index
     @comics = Marvel::Fetch.comics(fetch_comics_params)
+  end
+
+  def add_favorite
+    return not_acceptable('Param comic_id is required') if comic_params[:comic_id].blank?
+    return not_acceptable('You already has this comic as a favorite') if current_user.favorite_comics_ids.include?(comic_params[:comic_id].to_i)
+
+    current_user.favorite_comics_ids << comic_params[:comic_id].to_i
+    current_user.save!
+
+    render status: :no_content, json: {}
+  end
+
+  def remove_favorite
+    return not_acceptable('Param comic_id is required') if comic_params[:comic_id].blank?
+    return not_acceptable('You has not this comic as a favorite') unless current_user.favorite_comics_ids.include?(comic_params[:comic_id].to_i)
+
+    current_user.favorite_comics_ids.delete(comic_params[:comic_id].to_i)
+    current_user.save!
+
+    render status: :no_content, json: {}
   end
 
   private
 
   def comic_params
-    params.permit(:character, :page)
+    params.permit(:character, :page, :comic_id)
   end
 
   def fetch_comics_params
     { character_name: comic_params[:character], page: comic_params[:page] }.compact
+  end
+
+  def not_acceptable(error_message)
+    render status: :not_acceptable, json: { message: error_message }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'comics#index'
+  get 'add_favorite' => 'comics#add_favorite'
+  get 'remove_favorite' => 'comics#remove_favorite'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
